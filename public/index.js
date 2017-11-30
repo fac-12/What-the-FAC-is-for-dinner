880994439afe5aeefd0b7573b34c899a788628fa
Jamie Coe <jamiecoe66@gmail.com>
2017-11-30 15:00:07 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
 /*eslint-disable*/
 
-// var xhr = new XMLHttpRequest();
-//
-// xhr.onreadystatechange = function() {
-//   if(this.readyState == 4 && this.status == 200){
-//     var obj = JSON.parse(xhr.responseText);
-//   }
-// }
-// xhr.open("GET", "/getDishes", true);
-// xhr.send();
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200){
+    var obj = JSON.parse(xhr.responseText);
+  }
+}
+xhr.open("GET", "/getDishes", true);
+xhr.send();
