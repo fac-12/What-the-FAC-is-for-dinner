6bdcba3904fe91025f1c0db604cd4075f744ddb8
Lucy King <lucy.m.king92@gmail.com>
2017-12-01 11:43:16 +0000

    Added aggregation of diet info and changed test script to define NODE_ENV=test

--- src/queries/getDishes.js
+++ src/queries/getDishes.js
@@ -2,7 +2,11 @@ const databaseConnection = require('../database/db_connections.js');
 
 const getDishes = (cb) => {
   databaseConnection.query(`
-    SELECT users.name AS users, users.gitterhandle AS gitter, dishes.name AS dishes, dietary.name AS diet FROM users, dishes, dietary, connections WHERE users.id = dishes.makerid AND dishes.id = connections.dish_id AND connections.dietary_id = dietary.id`, (err, res) => {
+    SELECT users.name AS users, users.gitterhandle AS gitter,
+    dishes.name AS dishes, string_agg(dietary.name, ', ') AS diet
+    FROM users, dishes, dietary, connections WHERE users.id = dishes.makerid
+    AND dishes.id = connections.dish_id AND connections.dietary_id = dietary.id
+    GROUP BY users.name, users.gitterhandle, dishes.name`, (err, res) => {
     if (err) {
       console.log('there is error');
       cb(err);
